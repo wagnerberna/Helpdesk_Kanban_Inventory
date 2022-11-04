461f386706a1af5c8045530ad6c964f8a286e531
Wagner Berna <72299498+wagnerberna@users.noreply.github.com>
2022-11-04 14:07:54 -0300

    report support graph

--- helpdesk/router/urls_support.py
+++ helpdesk/router/urls_support.py
@@ -5,6 +5,7 @@ from helpdesk.views.support import (
     support_view_list_open,
     support_view_update,
 )
+from helpdesk.views.support_report import report_by_techinical
 
 urlpatterns = [
     path("support/<int:id>/", support_view_update, name="support_update"),
@@ -15,4 +16,5 @@ urlpatterns = [
         name="support_by_technical",
     ),
     path("support_done/", support_view_list_done, name="support_list_done"),
+    path("report_by_techinical/", report_by_techinical, name="report_by_techinical"),
 ]
---
+++ helpdesk/views/support_report.py
@@ -0,0 +1,47 @@
+import base64
+import io
+import urllib
+
+import matplotlib.pyplot as plt
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
+from helpdesk.api.serializers import SupportFilterSerializer
+from helpdesk.forms import SupportFormUpdate, SupportFormUpdateView
+from helpdesk.models import Demand, Support
+from helpdesk.service.check_user_access import check_user_access
+
+
+@login_required
+def report_by_techinical(request):
+    try:
+        check_access = check_user_access(request)
+        if not check_access:
+            return redirect("access_denied")
+
+        plt.plot(range(10))
+        plt.title("Chamados por Técnico")
+        fig = plt.gcf()
+        # convert graph into dtring buffer and then we convert 64 bit code into image
+        buf = io.BytesIO()
+        fig.savefig(buf, format="png")
+        buf.seek(0)
+        string = base64.b64encode(buf.read())
+        context = {"data": urllib.parse.quote(string)}
+        # return render(request,'home.html',{'data':uri})
+
+        #     demands = Demand.objects.filter(status__name="Finalizado").order_by("-id")
+        #     demand_filter = SupportFilterSerializer(request.GET, queryset=demands)
+
+        #     # print(demand_filter)
+        #     context = {"all_demands": demands, "demand_filter": demand_filter}
+        template_path = "helpdesk/pages/report_by_Techinical.html"
+
+        return render(
+            request,
+            template_path,
+            context,
+        )
+    except Exception as error:
+        print("Internal error:", error)
+        raise
