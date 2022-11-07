4dda618ce996a0d02f0bb0b39aaebf48d034e5b3
Wagner Berna <72299498+wagnerberna@users.noreply.github.com>
2022-11-07 16:53:36 -0300

    task delete update

--- kanban/routes/urls_task.py
+++ kanban/routes/urls_task.py
@@ -1,9 +1,24 @@
 from django.urls import path
 from kanban.views.kanban import manager_view
-from kanban.views.task import task_view_create
+from kanban.views.task import (
+    task_view_create,
+    task_view_done,
+    task_view_open,
+    task_view_update,
+)
 
 urlpatterns = [
     path("task_add/", task_view_create, name="task_add"),
-    path("task_open/", manager_view, name="task_open_filter"),
-    path("task_done/", manager_view, name="task_done_filter"),
+    path("task_open/", task_view_open, name="task_open_filter"),
+    path("task_done/", task_view_done, name="task_done_filter"),
+    path(
+        "task_update/<int:id>/",
+        task_view_update,
+        name="task_update",
+    ),
+    path(
+        "task_delete/<int:id>/",
+        task_view_update,
+        name="task_delete",
+    ),
 ]
--- kanban/views/task.py
+++ kanban/views/task.py
@@ -30,3 +30,102 @@ def task_view_create(request):
     except Exception as error:
         print("Internal error:", error)
         raise
+
+
+@login_required
+def task_view_open(request):
+    try:
+        check_access = check_user_access(request)
+        if not check_access:
+            return redirect("access_denied")
+
+        tasks = Task.objects.all().order_by("-id").exclude(status__name="DONE")
+        context = {"tasks": tasks}
+        template_path = "kanban/pages/task_open_list.html"
+
+        return render(
+            request,
+            template_path,
+            context,
+        )
+
+    except Exception as error:
+        print("Internal error:", error)
+        raise
+
+
+@login_required
+def task_view_done(request):
+    try:
+        check_access = check_user_access(request)
+        if not check_access:
+            return redirect("access_denied")
+
+        tasks = Task.objects.filter(status__name="DONE").order_by("-id")
+        context = {"tasks": tasks}
+        template_path = "kanban/pages/task_done_list.html"
+
+        return render(
+            request,
+            template_path,
+            context,
+        )
+
+    except Exception as error:
+        print("Internal error:", error)
+        raise
+
+
+@login_required
+def task_view_update(request, id):
+    try:
+        check_access = check_user_access(request)
+        if not check_access:
+            return redirect("access_denied")
+
+        task = get_object_or_404(Task, pk=id)
+
+        form = TaskForm(request.POST or None, instance=task)
+
+        context = {"form": form}
+        template_path = "kanban/pages/task_update.html"
+
+        if form.is_valid():
+            form.save()
+            return redirect("task_open")
+
+        return render(
+            request,
+            template_path,
+            context,
+        )
+
+    except Exception as error:
+        print("Internal error:", error)
+        raise
+
+
+@login_required
+def task_view_delete(request, id):
+    try:
+        check_access = check_user_access(request)
+        if not check_access:
+            return redirect("access_denied")
+
+        task = get_object_or_404(Task, pk=id)
+        context = {"project": task}
+        template_path = "kanban/pages/task_delete.html"
+
+        if request.method == "POST":
+            task.delete()
+            return redirect("task_open_filter")
+
+        return render(
+            request,
+            template_path,
+            context,
+        )
+
+    except Exception as error:
+        print("Internal error:", error)
+        raise
