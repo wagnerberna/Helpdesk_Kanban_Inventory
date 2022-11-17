5fb5c38410596d09d3f4b039572fd09abc63a040
Wagner Berna <72299498+wagnerberna@users.noreply.github.com>
2022-11-17 15:58:32 -0300

    graphic project

--- ti/service/make_graphics.py
+++ ti/service/make_graphics.py
@@ -27,8 +27,11 @@ def make_graphic_barh(title, color, projects_names, project_percent_to_graphic):
     plt.barh(projects_names, project_percent_to_graphic, color=color)
 
     plt.title(title)
-    plt.rcParams["font.size"] = "16"
+    plt.rcParams["font.size"] = "18"
+
     fig = plt.gcf()
+    fig.set_size_inches(18, 4, forward=True)
+    fig.set_dpi(100)
     buf = io.BytesIO()
     fig.savefig(buf, format="png")
     buf.seek(0)
