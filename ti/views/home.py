db7fedecac7d655b43746863f51949e2893021dc
Wagner Berna <72299498+wagnerberna@users.noreply.github.com>
2022-11-18 16:56:29 -0300

    test login

--- ti/views/home.py
+++ ti/views/home.py
@@ -1,9 +1,33 @@
-from django.contrib.auth import logout, update_session_auth_hash
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.shortcuts import redirect, render
 from helpdesk.models import Support
 from ti.service.check_user_access import check_user_access
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(
+        request=request,
+        template_name="registration/login.html",
+        context={"form": form},
+    )
 
 
 @login_required
