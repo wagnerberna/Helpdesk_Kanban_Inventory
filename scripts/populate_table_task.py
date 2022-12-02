a67134373f5240c5bbb145c862f4e26016877dba
Wagner Berna <72299498+wagnerberna@users.noreply.github.com>
2022-12-02 07:54:53 -0300

    Update populate_table_task.py

--- scripts/populate_table_task.py
+++ scripts/populate_table_task.py
@@ -24,48 +24,45 @@ def run():
 
             project_id_find = Project.objects.filter(name=project_table)
             project_id = project_id_find.values("id")[0].get("id")
-            print(project_table, project_id)
+            # print(project_table, project_id)
 
             category_id_find = Category.objects.filter(name=category_table)
             category_id = category_id_find.values("id")[0].get("id")
-            print(category_table, category_id)
+            # print(category_table, category_id)
 
             status_id_find = Status.objects.filter(name=status_table)
             status_id = status_id_find.values("id")[0].get("id")
-            print(status_table, status_id)
+            # print(status_table, status_id)
 
-            print("task_owner_table:::", task_owner_table)
-            if math.isnan(task_owner_table):
-                print("ELSE!!!!")
-                # task_owner_id = None
-
-                Task.objects.create(
-                    title=title_table,
-                    project=Project.objects.get(id=project_id),
-                    category=Category.objects.get(id=category_id),
-                    # task_owner=None,
-                    status=Status.objects.get(id=status_id),
-                    description=description_table,
-                )
-
-            else:
+            if task_owner_table != "None":
                 user_task_owner_id_find = User.objects.filter(username=task_owner_table)
                 user_task_owner_id = user_task_owner_id_find.values("id")[0].get("id")
-                print(task_owner_table, user_task_owner_id)
+                # print(task_owner_table, user_task_owner_id)
 
                 task_owner_id_find = Team.objects.filter(user_name=user_task_owner_id)
                 task_owner_id = task_owner_id_find.values("id")[0].get("id")
+                task_owner = Team.objects.get(id=task_owner_id)
+                # print(task_owner_table, task_owner_id)
 
-                print(task_owner_table, task_owner_id)
+            else:
+                task_owner = None
 
-                Task.objects.create(
-                    title=title_table,
-                    project=Project.objects.get(id=project_id),
-                    category=Category.objects.get(id=category_id),
-                    task_owner=Team.objects.get(id=task_owner_id),
-                    status=Status.objects.get(id=status_id),
-                    description=description_table,
-                )
+            print(
+                title_table,
+                project_id,
+                category_id,
+                task_owner,
+                status_id,
+                description_table,
+            )
+            Task.objects.create(
+                title=title_table,
+                project=Project.objects.get(id=project_id),
+                category=Category.objects.get(id=category_id),
+                task_owner=task_owner,
+                status=Status.objects.get(id=status_id),
+                description=description_table,
+            )
 
     except Exception as error:
         print("Internal error:", error)
