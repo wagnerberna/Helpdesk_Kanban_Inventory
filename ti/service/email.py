1200731630c10aac406a9696d34fa22141051294
Wagner Berna <72299498+wagnerberna@users.noreply.github.com>
2023-02-14 11:14:38 -0300

    Update email.py

--- ti/service/email.py
+++ ti/service/email.py
@@ -5,14 +5,16 @@ from django.core.mail import send_mail
 def send_email(recipient_list, subject, message):
     try:
         EMAIL_SUPPORT = config("EMAIL_SUPPORT")
+        EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+
         send_mail(
             subject=subject,
             message=message,
             from_email=EMAIL_SUPPORT,
             recipient_list=recipient_list,
-            auth_user=EMAIL_SUPPORT,
+            auth_user=EMAIL_HOST_USER,
             auth_password=config("EMAIL_HOST_PASSWORD"),
-            fail_silently=False,
+            fail_silently=True,
         )
         print("send mail!!!!")
 
