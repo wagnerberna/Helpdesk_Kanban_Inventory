daf4edc51a756d99b1276e9e6e9bc90de40beb8c
Wagner Berna <72299498+wagnerberna@users.noreply.github.com>
2023-02-14 07:44:03 -0300

    config send mail

--- ti/service/mail.py
+++ ti/service/mail.py
@@ -1,19 +1,20 @@
 from decouple import config
-from django.core.mail import EmailMessage
+from django.core.mail import send_mail
 
 
-def send_mail():
+def send_mail2():
     try:
         MAIL_TEST = config("MAIL_TEST")
-        mail = EmailMessage(
+        send_mail(
             subject="title",
+            message="teste teste",
             from_email=MAIL_TEST,
-            to=[MAIL_TEST],
-            body="Chamado Criado com sucesso",
-            headers={"Replay-To": MAIL_TEST},
+            recipient_list=[MAIL_TEST],
+            auth_user=MAIL_TEST,
+            auth_password=config("EMAIL_HOST_PASSWORD"),
             fail_silently=False,
         )
-        mail.send()
+        print("send mail!!!!")
 
     except Exception as error:
         print("Internal error:", error)
