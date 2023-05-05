c75857e2b79423bc82944acb9fcedb5c6ad5b75c
Wagner Berna <72299498+wagnerberna@users.noreply.github.com>
2023-05-05 09:13:55 -0300

    finish priority task

--- kanban/forms.py
+++ kanban/forms.py
@@ -18,13 +18,22 @@ class ProjectForm(ModelForm):
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = ["title", "project", "category", "task_owner", "status", "description"]
+        fields = [
+            "title",
+            "project",
+            "category",
+            "task_owner",
+            "status",
+            "priority",
+            "description",
+        ]
         labels = {
             "title": "Título:",
             "project": "Projeto:",
             "category": "Categoria:",
             "task_owner": "Dono da Tarefa:",
             "status": "Status:",
+            "priority": "Prioridade",
             "description": "Descrição:",
         }
 
