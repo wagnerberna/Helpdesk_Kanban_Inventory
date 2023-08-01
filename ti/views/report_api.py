10fec5df1b04552e2e36fd9a036831150220242e
Wagner Berna <72299498+wagnerberna@users.noreply.github.com>
2023-08-01 10:27:52 -0300

    Update report_api.py

--- ti/views/report_api.py
+++ ti/views/report_api.py
@@ -293,10 +293,10 @@ def api_ocs_hardware(request):
         cpu_core_dual_count = int(df_ocs["cpu_type"].loc[df_ocs.cpu_type.str.contains("2 Duo|Dual|X4|Celeron")].count())
 
         # CPU Count por query:
-        cpu_core_i7_count = int(df_ocs.query("cpu_type.str.contains('i7')")["cpu_type"].count())
-        cpu_core_i5_count = int(df_ocs.query("cpu_type.str.contains('i5')")["cpu_type"].count())
-        cpu_core_i3_count = int(df_ocs.query("cpu_type.str.contains('i3')")["cpu_type"].count())
-        cpu_core_dual_count = int(df_ocs.query("cpu_type.str.contains('2 Duo|Dual|X4|Celeron')")["cpu_type"].count())
+        # cpu_core_i7_count = int(df_ocs.query("cpu_type.str.contains('i7')")["cpu_type"].count())
+        # cpu_core_i5_count = int(df_ocs.query("cpu_type.str.contains('i5')")["cpu_type"].count())
+        # cpu_core_i3_count = int(df_ocs.query("cpu_type.str.contains('i3')")["cpu_type"].count())
+        # cpu_core_dual_count = int(df_ocs.query("cpu_type.str.contains('2 Duo|Dual|X4|Celeron')")["cpu_type"].count())
 
         cpu_labels = ["i7", "i5", "i3", "Dual"]
         cpu_counts = [cpu_core_i7_count, cpu_core_i5_count, cpu_core_i3_count, cpu_core_dual_count]
@@ -312,15 +312,15 @@ def api_ocs_hardware(request):
 
         # memory_equal_3gb = len(df_ocs.loc[df_ocs.memory <= 4096])
         # memory_equal_6gb = len(df_ocs.loc[df_ocs.memory == 6144])
-        memory_equal_or_less_4gb = int(df_ocs["memory"].loc[df_ocs.memory <= 4096].count())
-        memory_between_6gb_and_8gb = int(df_ocs["memory"].loc[(df_ocs.memory >= 6144) & (df_ocs.memory <= 8192)].count())
+        memory_between_4gb_and_6gb = int(df_ocs["memory"].loc[df_ocs.memory <= 6144].count())
+        memory_equal_8gb = int(df_ocs["memory"].loc[(df_ocs.memory == 8192)].count())
         memory_between_12g_and_16gb = int(df_ocs["memory"].loc[(df_ocs.memory >= 12288) & (df_ocs.memory <= 16384)].count())
         memory_between_20gb_and_32gb = int(df_ocs["memory"].loc[(df_ocs.memory >= 20480) & (df_ocs.memory <= 32768)].count())
         # memory_equal_16gb = len(df_ocs.loc[df_ocs.memory == 16384])
         # memory_equal_32gb = len(df_ocs.loc[df_ocs.memory > 20048])
 
-        memory_labels = ["Entre 20GB e 32GB", "Entre 12GB e 16GB", "Entre 6GB e 8GB", "Entre 3GB e 4GB"]
-        memory_counts = [memory_between_20gb_and_32gb, memory_between_12g_and_16gb, memory_between_6gb_and_8gb, memory_equal_or_less_4gb]
+        memory_labels = ["Entre 20GB e 32GB", "Entre 12GB e 16GB", "Entre 8GB e 12GB", "Entre 4GB e 6GB"]
+        memory_counts = [memory_between_20gb_and_32gb, memory_between_12g_and_16gb, memory_equal_8gb, memory_between_4gb_and_6gb]
 
         context = {
             "cpu_labels": cpu_labels,
@@ -347,10 +347,10 @@ def api_ocs_department(request):
         df_ocs = pd.read_excel("doc/ocs_hosts_department.xlsx")
         df_departaments = pd.read_excel("doc/departments.xlsx")
         
-        departments = df_departaments.values.tolist()
+        departments = df_departaments["setor"].tolist()
         print(departments)
         cpu_labels = ["i7", "i5", "i3", "Dual"]
-        memory_labels = ["Entre 20GB e 32GB", "Entre 12GB e 16GB", "Entre 6GB e 8GB", "Entre 3GB e 4GB"]
+        memory_labels = ["Entre 20GB e 32GB", "Entre 12GB e 16GB", "Entre 8GB e 12GB", "Entre 4GB e 6GB"]
 
         cpu_core_i7_counts_all_departments = []
         cpu_core_i5_counts_all_departments = []
@@ -359,30 +359,30 @@ def api_ocs_department(request):
 
         memory_between_20gb_and_32gb_all_departments = [] 
         memory_between_12g_and_16gb_all_departments = [] 
-        memory_between_6gb_and_8gb_all_departments = [] 
-        memory_equal_or_less_4gb_all_departments = []
+        memory_equal_8gb_all_departments = [] 
+        memory_between_4gb_and_6gb_all_departments = []
         
         for department in departments:
 
-            cpu_core_i7_count = int(df_ocs.query("cpu_type.str.contains('i7')")["cpu_type"].count())
-            cpu_core_i5_count = int(df_ocs.query("cpu_type.str.contains('i5')")["cpu_type"].count())
-            cpu_core_i3_count = int(df_ocs.query("cpu_type.str.contains('i3')")["cpu_type"].count())
-            cpu_core_dual_count = int(df_ocs.query("cpu_type.str.contains('2 Duo|Dual|X4|Celeron')")["cpu_type"].count())
+            cpu_core_i7_count = int(df_ocs["cpu_type"].loc[df_ocs.cpu_type.str.contains("i7") & (df_ocs.department.str.contains(department))].count())
+            cpu_core_i5_count = int(df_ocs["cpu_type"].loc[df_ocs.cpu_type.str.contains("i5") & (df_ocs.department.str.contains(department))].count())
+            cpu_core_i3_count = int(df_ocs["cpu_type"].loc[df_ocs.cpu_type.str.contains("i3") & (df_ocs.department.str.contains(department))].count())
+            cpu_core_dual_count = int(df_ocs["cpu_type"].loc[df_ocs.cpu_type.str.contains("2 Duo|Dual|X4|Celeron") & (df_ocs.department.str.contains(department))].count())
 
             cpu_core_i7_counts_all_departments.append(cpu_core_i7_count)
             cpu_core_i5_counts_all_departments.append(cpu_core_i5_count)
             cpu_core_i3_counts_all_departments.append(cpu_core_i3_count)
             cpu_core_dual_counts_all_departments.append(cpu_core_dual_count)
 
-            memory_equal_or_less_4gb = int(df_ocs["memory"].loc[df_ocs.memory <= 4096].count())
-            memory_between_6gb_and_8gb = int(df_ocs["memory"].loc[(df_ocs.memory >= 6144) & (df_ocs.memory <= 8192)].count())
-            memory_between_12g_and_16gb = int(df_ocs["memory"].loc[(df_ocs.memory >= 12288) & (df_ocs.memory <= 16384)].count())
-            memory_between_20gb_and_32gb = int(df_ocs["memory"].loc[(df_ocs.memory >= 20480) & (df_ocs.memory <= 32768)].count())
+            memory_between_4gb_and_6gb = int(df_ocs["memory"].loc[(df_ocs.memory <= 6144) & (df_ocs.department.str.contains(department))].count())
+            memory_equal_8gb = int(df_ocs["memory"].loc[(df_ocs.memory == 8192) & (df_ocs.department.str.contains(department))].count())
+            memory_between_12g_and_16gb = int(df_ocs["memory"].loc[(df_ocs.memory >= 12288) & (df_ocs.memory <= 16384) & (df_ocs.department.str.contains(department))].count())
+            memory_between_20gb_and_32gb = int(df_ocs["memory"].loc[(df_ocs.memory >= 20480) & (df_ocs.memory <= 32768) & (df_ocs.department.str.contains(department))].count())
 
-            memory_between_20gb_and_32gb_all_departments.append(memory_equal_or_less_4gb) 
-            memory_between_12g_and_16gb_all_departments.append(memory_between_6gb_and_8gb) 
-            memory_between_6gb_and_8gb_all_departments.append(memory_between_12g_and_16gb) 
-            memory_equal_or_less_4gb_all_departments.append(memory_between_20gb_and_32gb)
+            memory_between_20gb_and_32gb_all_departments.append(memory_between_20gb_and_32gb) 
+            memory_between_12g_and_16gb_all_departments.append(memory_between_12g_and_16gb) 
+            memory_equal_8gb_all_departments.append(memory_equal_8gb) 
+            memory_between_4gb_and_6gb_all_departments.append(memory_between_4gb_and_6gb)
 
         context = {
             "departments": departments,
@@ -394,8 +394,8 @@ def api_ocs_department(request):
             "memory_labels": memory_labels,
             "memory_between_20gb_and_32gb_all_departments":memory_between_20gb_and_32gb_all_departments, 
             "memory_between_12g_and_16gb_all_departments":memory_between_12g_and_16gb_all_departments, 
-            "memory_between_6gb_and_8gb_all_departments":memory_between_6gb_and_8gb_all_departments, 
-            "memory_equal_or_less_4gb_all_departments":memory_equal_or_less_4gb_all_departments,
+            "memory_equal_8gb_all_departments":memory_equal_8gb_all_departments, 
+            "memory_between_4gb_and_6gb_all_departments":memory_between_4gb_and_6gb_all_departments,
         }
 
         return JsonResponse(context)
